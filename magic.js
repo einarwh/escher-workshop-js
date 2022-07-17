1a3806485ca2be19898c52e811dd505db99c03e8
Einar W. Høst <einarwh@gmail.com>
2022-07-17 17:06:14 +0200

    clean-up

--- magic.js
+++ magic.js
@@ -1,3 +1,17 @@
+function createMirrorMapper(height) {
+  return (pt) => {
+    return { x: pt.x, y: height - pt.y };
+  };
+}
+
+function mirrorShapes(height, styledShapes) {
+  let mapper = createMirrorMapper(height);
+  let mirroredShapes = styledShapes.map((s) => {
+    return { style: s.style, shape: s.shape.transpose(mapper) };
+  });
+  return mirroredShapes;
+}
+
 function createMagicMapper(box) {
   let a = box.a;
   let b = box.b;
